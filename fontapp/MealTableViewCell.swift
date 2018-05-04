027a4c8b5d5957524d2ac90c142690960ebdb06c
bailing <bailing@MacBook-Pro.local>
2018-05-05 00:56:46 +0800

    学习UITableViewController

---
+++ fontapp/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  fontapp
+//
+//  Created by bailing on 2018/5/5.
+//  Copyright © 2018年 bailing. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
