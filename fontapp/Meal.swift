7366ca827354baa895b023e2acad04d51eabb383
bailing <bailing1123@live.cn>
2018-05-07 22:28:58 +0800

    学习UITableViewController

---
+++ fontapp/Meal.swift
@@ -0,0 +1,16 @@
+//
+//  Meal.swift
+//  fontapp
+//
+//  Created by bailing on 2018/5/6.
+//  Copyright © 2018年 bailing. All rights reserved.
+//
+
+import UIKit
+
+class Meal: NSObject {
+
+    init!(name: String, photo: UIImage?) {
+        <#code#>
+    }
+}
--- fontapp/MealTableViewCell.swift
+++ fontapp/MealTableViewCell.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
-
+   
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
